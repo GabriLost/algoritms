ed5c740f9ee549b24941a7f30c4ff5abb2861d0a
Калиберда Алексей <kaliberda@lanit.ru>
2023-05-01 16:47:31 +0300

    решение 111, ошибки в treeNode+визуализация дерева

--- src/test/java/leetcode/TreeNode.java
+++ src/test/java/leetcode/TreeNode.java
@@ -1,32 +1,103 @@
 package leetcode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class TreeNode {
-    int val;
+    Integer val;
     TreeNode left;
     TreeNode right;
 
     TreeNode() {
     }
 
-    TreeNode(int val) {
+    TreeNode(Integer val) {
         this.val = val;
     }
 
+    TreeNode(Integer val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
     TreeNode(Integer[] arr) {
-        TreeNode root = TreeNode.createTree(arr, 0);
+        TreeNode root = this.buildTree(arr);
         this.val = root.val;
         this.left = root.left;
         this.right = root.right;
     }
 
-    public static TreeNode createTree(Integer[] arr, int index) {
-        if (index >= arr.length || arr[index] == null) {
+    public TreeNode buildTree(Integer[] arr) {
+        if (arr == null || arr.length == 0) {
             return null;
         }
-        TreeNode root = new TreeNode(arr[index]);
-        root.left = createTree(arr, 2 * index + 1);
-        root.right = createTree(arr, 2 * index + 2);
+
+        TreeNode root = new TreeNode(arr[0]);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        int i = 1;
+        while (!queue.isEmpty() && i < arr.length) {
+            TreeNode curr = queue.poll();
+
+            if (arr[i] != null) {
+                curr.left = new TreeNode(arr[i]);
+                queue.offer(curr.left);
+            }
+            i++;
+            if (i < arr.length && arr[i] != null) {
+                curr.right = new TreeNode(arr[i]);
+                queue.offer(curr.right);
+            }
+            i++;
+        }
+
         return root;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        var queue = new LinkedList<TreeNode>();
+        queue.offer(this);
+        var depth = 1;
+        var empties = 0;
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currNode = queue.poll();
+
+                if (currNode != null) {
+                    sb.append("(" + currNode.val + ") ");
+                    if (currNode.left != null) {
+                        sb.append("|");
+                    } else {
+                        sb.append("");
+                        empties++;
+                    }
+                    if (currNode.right != null) {
+                        sb.append("\\");
+                    } else {
+                        sb.append("");
+                        empties++;
+                    }
+
+                    queue.offer(currNode.left);
+                    queue.offer(currNode.right);
+                }
+
+                // add spacing between nodes
+                if (i == levelSize-1) {
+                    sb.append("\n");
+                    sb.append("    ".repeat(empties));
+                }
+            }
+
+            depth++;
+        }
+        sb.append("\n");
+        return sb.toString();
     }
 
 }
---
+++ src/test/java/leetcode/_0111_MinimumDepthOfBinaryTree.java
@@ -0,0 +1,54 @@
+
+package leetcode;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.LinkedList;
+
+import org.junit.Test;
+
+public class _0111_MinimumDepthOfBinaryTree {
+
+    @Test
+    public void testSolution() {
+
+        var solution = new Solution();
+
+        Integer[] arr1 = { 3, 9, 20, null, null, 15, 7 };
+        Integer[] arr2 = { 1, null, 2, null, 3, null, 4, null, 5 };
+
+        assertEquals(2, solution.minDepth(new TreeNode(arr1)));
+        assertEquals(5, solution.minDepth(new TreeNode(arr2)));
+
+    }
+
+    class Solution {
+        public int minDepth(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+
+            var queue = new LinkedList<TreeNode>();
+
+            queue.add(root);
+            var depth = 1;
+
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                while (size > 0) {
+                    var node = queue.poll();
+                    if (node.left == null && node.right == null)
+                        return depth;
+                    if (node.left != null)
+                        queue.add(node.left);
+                    if (node.right != null)
+                        queue.add(node.right);
+                    size--;
+                }
+                depth++;
+            }
+
+            return depth;
+        }
+    }
+}
